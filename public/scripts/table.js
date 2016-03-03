8283c6d88a11fbe6dc65fe584b55e6822848d216
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-03-03 17:08:34 +0200

    Move all table HTML on React

--- public/scripts/table.js
+++ public/scripts/table.js
@@ -1,8 +1,48 @@
+var data = [
+  {rank: 1, instrumentcode: 8674, currency: "JPY", buy: 0.65, sell: 0.18},
+  {rank: 2, instrumentcode: 8604, currency: "JPY", buy: 0.65, sell: 0.18},
+  {rank: 3, instrumentcode: 8664, currency: "JPY", buy: 0.65, sell: 0.18},
+  {rank: 4, instrumentcode: 8404, currency: "JPY", buy: 0.65, sell: 0.18},
+  {rank: 5, instrumentcode: 8904, currency: "JPY", buy: 0.65, sell: 0.18},
+  {rank: 6, instrumentcode: 8694, currency: "JPY", buy: 0.65, sell: 0.18}
+];
+
+var BestBuyTable = React.createClass({
+  render : function() {
+    return (
+      <table className="best_buy">
+          <caption>
+              Best-Buy
+          </caption>
+          <colgroup>
+              <col id="rank" />
+              <col id="instrument_code" />
+              <col id="currency" />
+              <col id="buy" />
+              <col id="sell" />
+          </colgroup>
+          <thead>
+              <tr>
+                  <th scope="col">Rank</th>
+                  <th scope="col">Instrument Code</th>
+                  <th scope="col">Currency</th>
+                  <th scope="col">Buy</th>
+                  <th scope="col">Sell</th>
+              </tr>
+          </thead>
+          <BestBuyTableRows />
+      </table>
+    );
+  }
+});
+
 var BestBuyTableRows = React.createClass({
   render: function() {
     return (
-      <BestBuyTableRow rank="2" instrumentcode="8604" currency="JPY" buy="0.65" sell="0.18">
-      </BestBuyTableRow>
+      <tbody>
+        <BestBuyTableRow rank="2" instrumentcode="8604" currency="JPY" buy="0.65" sell="0.18">
+        </BestBuyTableRow>
+      </tbody>
     );
   }
 });
@@ -22,6 +62,6 @@ var BestBuyTableRow = React.createClass({
 });
 
 ReactDOM.render(
-  <BestBuyTableRows />,
-  document.getElementById('best_buy_content')
+  <BestBuyTable />,
+  document.getElementById('best_buy')
 );

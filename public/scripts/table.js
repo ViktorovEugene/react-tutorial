19bf6cd957b3c72fb62a4161ad8cb6ff1246017f
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-03-03 14:51:18 +0200

    Make a rows as parent of row

--- public/scripts/table.js
+++ public/scripts/table.js
@@ -1,17 +1,27 @@
 var BestBuyTableRows = React.createClass({
   render: function() {
     return (
+      <BestBuyTableRow rank="2" instrumentcode="8604" currency="JPY" buy="0.65" sell="0.18">
+      </BestBuyTableRow>
+    );
+  }
+});
+
+var BestBuyTableRow = React.createClass({
+  render: function() {
+    return (
       <tr>
-          <td>1</td>
-          <td>8604</td>
-          <td>JPY</td>
-          <td>0.65</td>
-          <td>0.18</td>
+          <td>{this.props.rank}</td>
+          <td>{this.props.instrumentcode}</td>
+          <td>{this.props.currency}</td>
+          <td>{this.props.buy}</td>
+          <td>{this.props.sell}</td>
       </tr>
     );
   }
 });
+
 ReactDOM.render(
   <BestBuyTableRows />,
   document.getElementById('best_buy_content')
-)
+);

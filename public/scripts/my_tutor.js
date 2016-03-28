c48084698f2c1b26124cc8b0462b7f1f98bea97b
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-03-28 17:40:25 +0300

    Simple rendering as wrapped object

--- public/scripts/my_tutor.js
+++ public/scripts/my_tutor.js
@@ -1,15 +1,30 @@
 // tutorial's 1-th step
+var date = 1, tick = 2, timeStamp = 3;
+var data = {date: 1, tick: 2, timeStamp: 3};
 
 var CommentBox = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
+        <header>
+          <p><span>Date</span> { this.props.date } </p>
+          <p><span>Tick</span> { this.props.tick  } </p>
+          <p><span>TimeStamp</span> { this.props.timeStamp } </p>
+        </header>
     );
   }
 });
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
-);
+
+function ready() {
+    ReactDOM.render(
+      <CommentBox date={date} tick={tick} timeStamp={timeStamp}/>,
+      document.getElementById('content')
+    );
+  }
+
+$(document).ready(ready);
+$('.toggle').click(function () {
+  date++;
+  tick++;
+  timeStamp++;
+  ready();
+});

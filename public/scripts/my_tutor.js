e42948227b5b433b44742205df96d90d086dbb2d
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-03-29 19:47:45 +0300

    Step 8: Do the update every 2 seconds

--- public/scripts/my_tutor.js
+++ public/scripts/my_tutor.js
@@ -4,10 +4,7 @@ var data = [
 ];
 
 var CommentBox = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
+  loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -19,6 +16,13 @@ var CommentBox = React.createClass({
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
   render: function() {
     return (
@@ -77,6 +81,6 @@ var Comment = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox url="/api/comments" />,
+  <CommentBox url="/api/comments" pollInterval={2000} />,
   document.getElementById('content')
 );

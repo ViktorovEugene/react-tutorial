c6536e4fa6ff3a3da7f7162d0f2fa96e380d46a5
Eugene Viktorov <eugene.viktorov@anvil8.com>
2016-04-09 19:25:24 +0300

    Simple render items of an array

--- public/scripts/owners_and_childs.js
+++ public/scripts/owners_and_childs.js
@@ -1,19 +1,33 @@
+var data = [1,2,3,4,5,6,7,8,9,10];
+
 var MyContainer = React.createClass({
   getInitialState: function() {
     return {value: '0'}
   },
   render: function() {
-      return <MyChild value={this.state.value} />
+      console.log(this);
+      return <MyChild value={this.state.value}
+                      data={this.props.data} />
   }
 });
 
 var MyChild = React.createClass({
     render: function() {
-        return <p>{this.props.value}</p>
+      var items = this.props.data.map(function(element, index) {
+        return <p key={index}>{"data[" + index+ "]: " + element}</p>
+      });
+      console.log(items);
+      console.log(this.props.data);
+      return (
+        <main>
+          <h1>{"VAlue: " + this.props.value}</h1>
+          {items}
+        </main>
+      )
     }
 });
 
 ReactDOM.render(
-  <MyContainer />,
+  <MyContainer data={data} />,
   document.getElementById('content')
 );
